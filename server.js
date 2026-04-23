0968d11399d1d4148e4a190edbd3c3c8d597ae7c
ambatimaheshwarreddy5-jpg <ambatimaheshwarreddy5@gmail.com>
2026-04-23 20:54:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname));
+// Serve static files from current folder
+app.use(express.static(path.join(__dirname)));
 
+// Home route
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
